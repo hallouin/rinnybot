e75770f92e6c86bb65352894f9b1f5a22e584ade
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 17:51:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,13 @@ client.on('ready', () => {
     console.log('Rinnybot is here!');
 });
 
-client.on('message', message => {
-    if (message.content === (config.prefix + 'ping')) {
-    message.channel.sendMessage('Pong desu!');
+client.on("message", async message => {
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
   }
 });
 
