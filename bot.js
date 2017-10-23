e7687f02ecbb978ad38fa49de76a3fda9c178621
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 15:33:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,17 +5,18 @@ client.on('ready', () => {
     console.log('Rinnybot is here!');
 });
 
-client.on('message', message => {
-    if (message.content === 'r!ping') {
-    	message.channel.sendMessage('pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms');
-  	}
+ if(command === "r!ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
     
 });
     
 client.on('message', message => {
     if (message.content === 'r!commands') {
-const m = await message.channel.send("Ping?");
-    m.edit(`Pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    message.channel.sendMessage('Come back later!');
   }
     
 });
