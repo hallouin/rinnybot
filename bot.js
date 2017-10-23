5dc0fd5981563c5ad4280cca11215708164aed20
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 15:28:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,15 +7,27 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'r!ping') {
-    	message.reply('pong desu!');
+    	message.channel.sendMessage('pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms');
   	}
     
 });
     
 client.on('message', message => {
     if (message.content === 'r!commands') {
-    	message.channel.sendMessage('Come back later!');
-  	}
+const m = await message.channel.send("Ping?");
+    m.edit(`Pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+    
+  
+  if(command === "r!lenny") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join("( ͡° ͜ʖ ͡°)");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
     
 });
 
