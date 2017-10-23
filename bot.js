a012b230413754fe4d0a751ec45d38d502efe9b8
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 16:17:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
 
 client.on('ready', () => {
     console.log('Rinnybot is here!');
 });
 
 client.on('message', message => {
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+    
 if(command === 'ping') {
   message.channel.send('Pong!');
 } else 
