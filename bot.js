fe35887283348267790b6316519d1c82f23b3ebb
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 14:45:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,30 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Rinnybot is here!');
 });
 
 client.on('message', message => {
     if (message.content === 'r!ping') {
-    	message.reply('pong desu!');
+    	message.reply('pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`');
   	}
+});
+
+client.on('message', message => {
+    if (message.content === 'r!commands') {
+    	message.reply('Check back later! Rinnybot is still in the works');
+  	}
+});
+
+ if(command === "hentai") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join("Lewd!");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
