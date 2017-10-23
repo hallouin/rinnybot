70148375d0f196a09d2d58576051336e5d9e8acf
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 17:21:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,14 +36,9 @@ if( swearWords.some(word => message.content.includes(word)) ) {
 });
 
 client.on('message', message => {
-if (message.content === (config.prefix + 'lenny')) {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join('lenny face');
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
+const lenny = ["r!lenny"];
+if( lenny.some(word => message.content.includes(word)) ) {
+  message.channel.sendMessage("( ͡° ͜ʖ ͡°)");
 }
     
 });
