418be336763274c4fd89652ce79952c6958c8d9c
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 06:01:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'r!ping') {
+    	message.reply('pong desu!');
   	}
 });
 
