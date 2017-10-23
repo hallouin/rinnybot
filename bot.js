810e1bc054f9d4d32eeef171ed689b151f24ca27
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-23 16:20:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on('ready', () => {
     console.log('Rinnybot is here!');
 });
 
 client.on('message', message => {
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-    
-if(command === 'ping') {
-  message.channel.sendMessage('Pong!');
-} else 
-if (command === 'blah') {
-  message.channel.sendMessage('Meh.');
-}
+    if (message.content === 'r!ping') {
+const m = await message.channel.sendMessage("Ping?");
+    m.edit(`Pong desu! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+});
+
+client.on('message', message => {
+    if (message.content === 'r!commands') {
+    message.channel.sendMessage('Come back later!');
+  } 
     
 });
 
