5aa0d086fbecc044a23a109e50eb7c0df6051233
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-24 12:05:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,10 +65,9 @@ client.on('message', message => {
 
 };
     
-    message.author.sendMessage("Here are my list of commands!", { embed });
-  } 
+    message.author.sendMessage("Here are my list of commands!", { embed }); 
     message.channel.reply("Gotcha! Sent you a DM!");
-    
+} 
 });
 
 client.on('message', message => {
