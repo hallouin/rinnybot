fdbc8214e1af1be22351e89e3849cbee38ff0498
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-25 04:51:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const yt = require('ytdl-core');
 const client = new Discord.Client();
 const config = require("./config.json");
-const client = new Client();
 let queue = {};
 
 const commands = {
