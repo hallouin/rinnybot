2361c48ae4f40e37b7f7ed990424668506c4524d
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-25 05:03:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,98 +1,257 @@
 const Discord = require('discord.js');
-const yt = require('ytdl-core');
 const client = new Discord.Client();
 const config = require("./config.json");
-let queue = {};
-
-const commands = {
-	'play': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${config.prefix}add`);
-		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
-		if (queue[msg.guild.id].playing) return msg.channel.sendMessage('Already Playing');
-		let dispatcher;
-		queue[msg.guild.id].playing = true;
-
-		console.log(queue);
-		(function play(song) {
-			console.log(song);
-			if (song === undefined) return msg.channel.sendMessage('Queue is empty').then(() => {
-				queue[msg.guild.id].playing = false;
-				msg.member.voiceChannel.leave();
-			});
-			msg.channel.sendMessage(`Playing: **${song.title}** requested by: **${song.requester}**`);
-			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : config.passes });
-			let collector = msg.channel.createCollector(m => m);
-			collector.on('message', m => {
-				if (m.content.startsWith(config.prefix + 'pause')) {
-					msg.channel.sendMessage('paused').then(() => {dispatcher.pause();});
-				} else if (m.content.startsWith(config.prefix + 'resume')){
-					msg.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
-				} else if (m.content.startsWith(config.prefix + 'skip')){
-					msg.channel.sendMessage('skipped').then(() => {dispatcher.end();});
-				} else if (m.content.startsWith('volume+')){
-					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
-					msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-				} else if (m.content.startsWith('volume-')){
-					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
-					msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-				} else if (m.content.startsWith(config.prefix + 'time')){
-					msg.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
-				}
-			});
-			dispatcher.on('end', () => {
-				collector.stop();
-				play(queue[msg.guild.id].songs.shift());
-			});
-			dispatcher.on('error', (err) => {
-				return msg.channel.sendMessage('error: ' + err).then(() => {
-					collector.stop();
-					play(queue[msg.guild.id].songs.shift());
-				});
-			});
-		})(queue[msg.guild.id].songs.shift());
-	},
-	'join': (msg) => {
-		return new Promise((resolve, reject) => {
-			const voiceChannel = msg.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-		});
-	},
-	'add': (msg) => {
-		let url = msg.content.split(' ')[1];
-		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a YouTube video url, or id after ${config.prefix}add`);
-		yt.getInfo(url, (err, info) => {
-			if(err) return msg.channel.sendMessage('Invalid YouTube Link: ' + err);
-			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
-			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
-			msg.channel.sendMessage(`added **${info.title}** to the queue`);
-		});
-	},
-	'queue': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${config.prefix}add`);
-		let tosend = [];
-		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
-		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
-	},
-	'help': (msg) => {
-		let tosend = ['```xl', config.prefix + 'join : "Join Voice channel of msg sender"',	config.prefix + 'add : "Add a valid youtube link to the queue"', config.prefix + 'queue : "Shows the current queue, up to 15 songs shown."', config.prefix + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), config.prefix + 'pause : "pauses the music"',	config.prefix + 'resume : "resumes the music"', config.prefix + 'skip : "skips the playing song"', config.prefix + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
-		msg.channel.sendMessage(tosend.join('\n'));
-	},
-	'reboot': (msg) => {
-		if (msg.author.id == config.adminID) process.exit(); //Requires a node module like Forever to work.
-	}
-});
 
 client.on('ready', () => {
     console.log('Rinnybot is here!');
 });
 
+client.on('message', message => {
+    if (message.content === (config.prefix + 'ping')) {
+    message.channel.sendMessage('Pong desu!');
+  }
+});
 
 client.on('message', message => {
-	if (!msg.content.startsWith(config.prefix)) return;
-	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(config.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(config.prefix.length).split(' ')[0]](msg);
+    if (message.content === (config.prefix + 'commands')) {
+const embed = {
+  "title": "`r!ping`",
+  "description": "Responds with `Pong desu!` Will include numerical value for ping (ms) soon.",
+  "color": 9059670,
+  "fields": [
+    {
+      "name": "`r!commands` , `r!command` , `r!help`",
+      "value": "Displays a list of commands available for use with Rinnybot."
+    },
+       {
+      "name": "`r!yes`",
+      "value": "(ﾟｰﾟ)(｡_｡)"
+    },
+      {
+      "name": "`r!no`",
+      "value": "(･_･ 三 ･_･) "
+    },
+    {
+      "name": "`r!nope`",
+      "value": "=͟͟͞͞(　 ω )=͟͟͞͞　³ ³"
+    },
+    {
+      "name": "`r!teehee`",
+      "value": "(ﾉ≧ڡ≦)"
+    },
+    {
+      "name": "`r!gimme`",
+      "value": "(੭ु´･ω･`)੭ु⁾⁾"
+    },
+    {
+      "name": "`r!hug` or `r!hug @user`",
+      "value": "꒰๑*´ᗜ`*꒱*›◡‹꒱꒱"
+    }
+  ]
+
+};
+    message.delete(100)
+    message.author.sendMessage("Here are my list of commands!", { embed }); 
+} 
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'command')) {
+const embed = {
+  "title": "`r!ping`",
+  "description": "Responds with `Pong desu!` Will include numerical value for ping (ms) soon.",
+  "color": 9059670,
+  "fields": [
+    {
+      "name": "`r!commands` , `r!command` , `r!help`",
+      "value": "Displays a list of commands available for use with Rinnybot."
+    },
+       {
+      "name": "`r!yes`",
+      "value": "(ﾟｰﾟ)(｡_｡)"
+    },
+      {
+      "name": "`r!no`",
+      "value": "(･_･ 三 ･_･) "
+    },
+    {
+      "name": "`r!nope`",
+      "value": "=͟͟͞͞(　 ω )=͟͟͞͞　³ ³"
+    },
+    {
+      "name": "`r!teehee`",
+      "value": "(ﾉ≧ڡ≦)"
+    },
+    {
+      "name": "`r!gimme`",
+      "value": "(੭ु´･ω･`)੭ु⁾⁾"
+    },
+    {
+      "name": "`r!hug` or `r!hug @user`",
+      "value": "꒰๑*´ᗜ`*꒱*›◡‹꒱꒱"
+    }
+  ]
+
+};
+    message.delete(100)
+    message.author.sendMessage("Here are my list of commands!", { embed }); 
+} 
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'help')) {
+    const embed = {
+  "title": "`r!ping`",
+  "description": "Responds with `Pong desu!` Will include numerical value for ping (ms) soon.",
+  "color": 9059670,
+  "fields": [
+    {
+      "name": "`r!commands` , `r!command` , `r!help`",
+      "value": "Displays a list of commands available for use with Rinnybot."
+    },
+       {
+      "name": "`r!yes`",
+      "value": "(ﾟｰﾟ)(｡_｡)"
+    },
+      {
+      "name": "`r!no`",
+      "value": "(･_･ 三 ･_･) "
+    },
+    {
+      "name": "`r!nope`",
+      "value": "=͟͟͞͞(　 ω )=͟͟͞͞　³ ³"
+    },
+    {
+      "name": "`r!teehee`",
+      "value": "(ﾉ≧ڡ≦)"
+    },
+    {
+      "name": "`r!gimme`",
+      "value": "(੭ु´･ω･`)੭ु⁾⁾"
+    },
+    {
+      "name": "`r!hug` or `r!hug @user`",
+      "value": "꒰๑*´ᗜ`*꒱*›◡‹꒱꒱"
+    }
+  ]
+
+};
+    message.delete(100)
+    message.author.sendMessage("Here are my list of commands!", { embed }); 
+} 
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'yes')) {
+   const embed = {
+  "image": {
+    "url": "http://25.media.tumblr.com/tumblr_mbqui4CyUm1riu78lo1_500.gif"
+  }
+};
+message.channel.sendMessage("Yes!", { embed });
+}
+    
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'no')) {
+   const embed = {
+  "image": {
+    "url": "http://media.giphy.com/media/VYwLv746owIkU/giphy.gif"
+  }
+};
+message.channel.sendMessage("No!", { embed });
+}
+    
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'nope')) {
+   const embed = {
+  "image": {
+    "url": "https://media.giphy.com/media/pXPBrb74aJhUQ/giphy.gif"
+  }
+};
+message.channel.sendMessage("NOPE NOPE NOPE", { embed });
+}
+    
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'teehee')) {
+   const embed = {
+  "image": {
+    "url": "http://pa1.narvii.com/5591/2efc311fd908344b01ed7a3723d8f066bb267246_hq.gif"
+  }
+};
+message.channel.sendMessage("TEEHEE (ﾉ≧ڡ≦)", { embed });
+}
+    
+});
+
+client.on('message', message => {
+    if (message.content === (config.prefix + 'gimme')) {
+   const embed = {
+  "image": {
+    "url": "http://data.whicdn.com/images/82274594/original.gif"
+  }
+};
+message.channel.sendMessage({ embed });
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["r!hug"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+   const embed = {
+  "image": {
+    "url": "https://media.tenor.com/images/08de7ad3dcac4e10d27b2c203841a99f/tenor.gif"
+  }
+};
+message.channel.sendMessage({ embed });
+}
+    
+});
+
+client.on('message', message => {
+    if (message.author.bot) return;
+const swearWords = ["hentai", "ochinchin", "oppai", "boku no pico", "ecchi", 'chinchin', 'chinpo', 'panties', 'pantyshot', 'lewd', 'Hentai', 'HENTAI', 'Ochinchin', 'OCHINCHIN', 'Oppai', 'OPPAI', 'Boku no Pico', 'BOKU NO PICO', 'Boku no pico', 'Ecchi', 'ECCHI', 'panty shot', 'PANTIES', 'PANTY SHOT', 'LEWD', 'LOOD', 'Lewd', 'Lood'];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  const embed = {
+  "image": {
+    "url": "https://i.imgur.com/XoYgHyi.gif"
+  }
+};
+message.channel.sendMessage("Lewd desu!", { embed });
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["i love rinnybot"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["I love rinnybot"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["i love Rinnybot"];
+if( swearWords.some(word => message.content.includconst Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on('ready', () => {
+    console.log('Rinnybot is here!');
 });
 
 client.on('message', message => {
@@ -336,6 +495,54 @@ if( swearWords.some(word => message.content.includes(word)) ) {
 client.on('message', message => {
 const swearWords = ["i love Rinnybot"];
 if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["I love Rinnybot"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["I LOVE RINNYBOT"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["I LOVE YOU RINNYBOT"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const swearWords = ["i love you rinnybot"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("I love you too! ♡");
+}
+    
+});
+
+client.on('message', message => {
+const lenny = ["r!lenny"];
+if( lenny.some(word => message.content === (word)) ) {
+  message.delete(100);
+  message.channel.sendMessage("( ͡° ͜ʖ ͡°)");
+}
+    
+});
+
+client.login(process.env.BOT_TOKEN);
+es(word)) ) {
   message.reply("I love you too! ♡");
 }
     
