2f36d7fc656ecb382e15a1788d17ace023d4cc6f
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-25 20:46:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ if (message.content.startsWith(config.prefix + 'hug')) {
 
   const imagesArray = [{ image:"http://gifimage.net/wp-content/uploads/2017/06/anime-hug-gif-1.gif" }];
 
-const rng = imagesArray[Math.floor(Math.random()*3)+1];
+const rng = imagesArray[Math.floor(Math.random()*1)+1];
 
 message.channel.sendMessage(rng);
 }
