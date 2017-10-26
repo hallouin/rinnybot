a574fe576bee5a3552c2ab76e9d83f4eca3b3f60
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-25 22:38:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -213,22 +213,22 @@ const embed = {
 }
 };
 
-message.channel.sendMessage(message.mentions.username + ", you got a hug from " + message.author.username + "!", { embed });
+message.channel.sendMessage("You got a hug from " + message.author.username + "!", { embed });
 }
 });
 
 client.on('message', message => {
     if (message.author.bot) return;
 const swearWords = ["hentai", "ochinchin", "oppai", "boku no pico", "ecchi", 'chinchin', 'chinpo', 'panties', 'pantyshot', 'lewd', 'Hentai', 'HENTAI', 'Ochinchin', 'OCHINCHIN', 'Oppai', 'OPPAI', 'Boku no Pico', 'BOKU NO PICO', 'Boku no pico', 'Ecchi', 'ECCHI', 'panty shot', 'PANTIES', 'PANTY SHOT', 'LEWD', 'LOOD', 'Lewd', 'Lood'];
-if( swearWords.some(word => message.content.includes(word)) ) {
-  const embed = {
+if( swearWords.some(word => message.content.includes(word)) ) {const imageArray = ["https://i.imgur.com/qknrvCO.gif", "http://gifimage.net/wp-content/uploads/2017/08/lewd-anime-gif-8-1.gif", "https://media.giphy.com/media/xnmArcgSzKbo4/giphy.gif", "http://gifimage.net/wp-content/uploads/2017/08/lewd-anime-gif-6-1.gif", "https://i.imgur.com/SaGnoAr.gif", "http://i0.kym-cdn.com/photos/images/original/000/746/820/fed.gif"];
+const embed = {
   "image": {
-    "url": "https://i.imgur.com/XoYgHyi.gif"
-  }
+    "url": (imageArray[rng])
+}
 };
+
 message.channel.sendMessage("Lewd desu!", { embed });
 }
-    
 });
 
 client.on('message', message => {
