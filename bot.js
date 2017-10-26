49955d609a53a4ed423ed700a0423da40ef28df2
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-25 23:08:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,35 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+if(message.author.bot) return;
+});
+if(!message.content.startsWith(prefix)) return;
+let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+if (command === "play"){
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel){
+      return message.channel.sendMessage(":x: You are not in a voice channel!!");
+    }
+	message.channel.sendMessage(":white_check_mark: **Connected!**");
+    voiceChannel.join()
+    .then(connection => {
+	const args = message.content.split(" ").slice(1);
+      let stream = yt(args.join(" "), {audioonly: true});
+      yt.getInfo(args.join(" "), function(err, info) {
+      const title = info.title
+	  message.channel.sendMessage(`Now playing \`${title}\``)
+      })
+      const dispatcher = connection.playStream(stream);
+      dispatcher.on('end', () => {
+         voiceChannel.leave();
+       }).catch(e =>{
+         console.error(e);
+       });
+    })
+  }
+
+client.on('message', message => {
     if (message.content === (config.prefix + 'commands')) {
 const embed = {
   "title": "`r!ping`",
