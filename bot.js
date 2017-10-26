755cec38301bca9f429a8ddb2298b2c4bbd52f17
hallouin <33024872+hallouin@users.noreply.github.com>
2017-10-25 21:29:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -206,14 +206,14 @@ client.on('message', message => {
 if (message.content.startsWith(config.prefix + 'hug')) {
 
 const rng = Math.floor(Math.random()*3)+1;
-const embed1 = {
+const embed = {
   "image": {
     "url": "http://data.whicdn.com/images/82274594/original.gif"
   }
 };
 
     if (rng === 1) {
-message.channel.sendMessage({ embed1 });
+message.channel.sendMessage({ embed });
     } else if (rng !== 1) {
 message.channel.sendMessage("Working on it!");
     }
