f56cad12cdf6908748fe4b53ed84caf64209aa3d
hallouin <33024872+hallouin@users.noreply.github.com>
2017-11-03 00:13:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -288,10 +288,9 @@ client.on('message', message => {
 if( message.content.startsWith(config.prefix + '8ball' ) {
     const rng = Math.floor(Math.random()*12)+1;
     const response = ["Surely it is so!", "Yep!", "For sure!", "I'm not so sure...", "Probably not.", "Nope, definitely not.", "I'm sure of it!", "Perhaps if you tell me you love Rinnybot, then yes!", "I can't really tell right now.", "No no no nope", "Of course!", "I think it's better not to say..."];
-}
-};
 
-message.channel.sendMessage("response[rng]");
+message.channel.sendMessage(response[rng]);
+
 }
 });
 
