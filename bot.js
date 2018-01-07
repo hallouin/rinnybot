a7896bf9553f8dd9781466259df40d38f0073c2f
hallouin <33024872+hallouin@users.noreply.github.com>
2018-01-07 15:05:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,15 @@ client.on('ready', () => {
     console.log('Rinnybot is here!');
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome, welcome, welcome, ${member}!`);
+});
+
 client.on('message', message => {
     if (message.mentions.bot) {
         message.channel.sendMessage("Thank you!");
