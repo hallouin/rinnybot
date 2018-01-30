f0735f85328894cb22cfa11ca4e822cbef691784
hallouin <33024872+hallouin@users.noreply.github.com>
2018-01-29 23:33:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 const yt = require('ytdl-core');
-const music = require('discord.js-music-v11');
-const token = "process.env.BOT_TOKEN"
 
 client.on('ready', () => {
     console.log('Rinnybot is here!');
