a31c0b0f96e299ccf0fee96ecff4b4e9636c0dbf
hallouin <33024872+hallouin@users.noreply.github.com>
2018-09-16 19:59:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
       const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
     
-    if(command === "rinnybotpleasesay") {
+    if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
