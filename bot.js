3ef81f0b74529522f9bc352d4b0aa42ddd323f85
hallouin <33024872+hallouin@users.noreply.github.com>
2018-09-21 20:49:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,15 +25,10 @@ client.on('message', message => {
 });
 
 client.on('message', function(message) {
-    if (message.content == "^clear") {
-        if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetchMessages()
-               .then(function(list){
-                    message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
-        }
+    if (command === "say") {
+        let messagecount = parseInt(numberofmessages);
+  message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
     }
-
 });
 
 client.on('message', message => {
